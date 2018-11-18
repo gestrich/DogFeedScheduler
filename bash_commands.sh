b162e2d35979b0ac57f43e22dcbdaf98d741f745
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2018-11-18 09:59:40 -0500

    Add bash file

---
+++ bash_commands.sh
@@ -0,0 +1,3 @@
+alias update="cd ~/Desktop/DogFeedScheduler; git --git-dir ~/Desktop/DogFeedScheduler/.git pull"
+alias build="cd ~/Desktop/DogFeedScheduler; g++ -Wall -o DogFeeder ~/Desktop/DogFeedScheduler/DogFeedScheduler/DogFeedScheduler/*.cpp -lwiringPi;"
+alias run="cd ~/Desktop/DogFeedScheduler; ./DogFeeder dog"
