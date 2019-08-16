e249ba7de7cb8f3a6f9baf9754e22bd81ec2b4c6
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2019-08-15 21:27:59 -0400

    Update bash

--- bash_commands.sh
+++ bash_commands.sh
@@ -1,9 +1,11 @@
 alias dogdir="cd ~/dev/DogFeedScheduler;"
 alias update="cd ~/dev/DogFeedScheduler; git --git-dir ~/dev/DogFeedScheduler/.git pull"
 if [ "$SPI_DISABLED" = "1" ]; then
-  alias build="cd ~/dev/DogFeedScheduler; g++ -Wall -std=c++11 -o DogFeeder ~/dev/DogFeedScheduler/*.cpp -D"WIRING_PI_DISABLED=${WIRING_PI_DISABLED}" -D"SPI_DISABLED=${SPI_DISABLED}";"
-  else
-  alias build="cd ~/dev/DogFeedScheduler; g++ -Wall -std=c++11 -o DogFeeder ~/dev/DogFeedScheduler/*.cpp ~/dev/DogFeedScheduler/libraries/RaspberryPi-mcp3008Spi/mcp3008Spi.cpp -lwiringPi -D"WIRING_PI_DISABLED=${WIRING_PI_DISABLED}";"
+	alias build="cd ~/dev/DogFeedScheduler; g++ -Wall -std=c++11 -o DogFeeder ~/dev/DogFeedScheduler/*.cpp -D"WIRING_PI_DISABLED=${WIRING_PI_DISABLED}" -D"SPI_DISABLED=${SPI_DISABLED}";"
+else
+	#This part -D"WIRING_PI_DISABLED=${WIRING_PI_DISABLED} is still setting WIRING_PI_DISABLED even when not defined
+	#alias build="cd ~/dev/DogFeedScheduler; g++ -Wall -std=c++11 -o DogFeeder ~/dev/DogFeedScheduler/*.cpp ~/dev/DogFeedScheduler/libraries/RaspberryPi-mcp3008Spi/mcp3008Spi.cpp -lwiringPi -D"WIRING_PI_DISABLED=${WIRING_PI_DISABLED}";"
+	alias build="cd ~/dev/DogFeedScheduler; g++ -Wall -std=c++11 -o DogFeeder ~/dev/DogFeedScheduler/*.cpp ~/dev/DogFeedScheduler/libraries/RaspberryPi-mcp3008Spi/mcp3008Spi.cpp -lwiringPi;"
 fi 
 
 alias run="cd ~/dev/DogFeedScheduler; ./DogFeeder $1"
